9679ae9a7e208a8f65d1200de17240316577669d
fatima <162475613+fatimaRiaz531@users.noreply.github.com>
2024-03-09 15:56:36 +0500

    exercise3

--- app.ts
+++ app.ts
@@ -1,2 +1,9 @@
-let personName: string= "ERIC";
-console.log(`hellow ${personName} would you like to learn some python today?`);
+//lower case
+let personName: string ="Fatima"
+console.log("lowercase:", personName.toLowerCase());
+
+//upper case
+console.log("uppercase:", personName.toLocaleUpperCase());
+
+//title case
+console.log("titlecase:", personName.replace(/\b\w/g,c => c.toUpperCase()));
